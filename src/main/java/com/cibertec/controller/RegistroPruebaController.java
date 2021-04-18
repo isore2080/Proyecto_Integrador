db784e5eece02220cccb23a677f64fb639013eda
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-04-18 12:12:46 -0500

    Se agrega registro prueba

---
+++ src/main/java/com/cibertec/controller/RegistroPruebaController.java
@@ -0,0 +1,13 @@
+package com.cibertec.controller;
+
+/**
+ * @author JORGE JACINTO
+ * 
+ */
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegistroPruebaController {
+
+}
